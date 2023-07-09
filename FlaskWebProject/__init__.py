447e8e8143cdbe834e2faf62f7055d5aef544ff9
Thanh NguyenCong <nguyencongthanh200199@gmail.com>
2023-07-09 15:42:30 +0700

    Add Warning Logs

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -9,12 +9,24 @@ from flask_login import LoginManager
 from flask_session import Session
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+wsgi_app =  app.wsgi_app
+
+app.logger.setLevel(logging.WARNING)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+
+app.logger.setLevel(logging.INFO)
+streamHandler.setLevel(logging.INFO)
+streamHandler = logging.StreamHandler()
+
+app.logger.addHandler(streamHandler)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
-app.logger.info('%s logged in successfully', login)
 login.login_view = 'login'
 
 import FlaskWebProject.views
